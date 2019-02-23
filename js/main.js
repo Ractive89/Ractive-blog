30dfc4877c5b053b3e3d36bbbf84f24ce2f2c6d3
unknown <ractive89@126.com>
2019-02-24 03:25:00 +0800

    Site updated: 2019-02-24 03:24:56

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){  
+    // copy from https://github.com/fi3ework/hexo-theme-archer
+    var logStyle = 'color: #fff; background: #f75357; padding: 1px; border-radius: 5px;'
+}) 
